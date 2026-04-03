17fd2187cb2bab660343f4db69576b64f02d1dc5
Zainab937-gif <zainab.aamir345@gmail.com>
2026-04-04 00:42:04 +0500

    Create post.js

---
+++ post.js
@@ -0,0 +1,10 @@
+const posts = [
+  {
+    title: "My First Post",
+    content: "This is my first blog post!"
+  },
+  {
+    title: "Learning Web Development",
+    content: "Today I learned HTML, CSS and JS."
+  }
+];
